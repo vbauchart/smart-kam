429e0df9e868228a4bf7ff84bc9cc4dae31452e3
Vincent Bauchart <vincent-externe.bauchart@dalkia.fr>
2026-03-16 23:46:50 +0100

    import Torchbald

---
+++ Torchebald.mm
@@ -0,0 +1,57 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1400016602850" ID="ID_438224758" MODIFIED="1400016722259" TEXT="Torchebald">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1400016662381" ID="ID_1287839381" MODIFIED="1400016722253" POSITION="right" TEXT="LOI">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1400016740867" ID="ID_1310533082" MODIFIED="1400016752690" TEXT="Letter of intention">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1400017440164" ID="ID_623006308" MODIFIED="1400017441247" TEXT="MCD">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1400017077314" ID="ID_511391498" MODIFIED="1400017443761" TEXT="Reference">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1400017230364" ID="ID_1491462768" MODIFIED="1400017443762" TEXT="Reference d&apos;un meme produit different selon les client">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1400017114342" ID="ID_576701166" MODIFIED="1400017443763" TEXT="packaging different selon les references">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1400017128906" ID="ID_1673871013" MODIFIED="1400017226018" TEXT="ex: livraison Bresil ou France"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1400017388644" ID="ID_1022149384" MODIFIED="1400017576748" TEXT="Tableau des Incoterm">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1400017397989" ID="ID_552164995" MODIFIED="1400017443770" TEXT="liste des differents mode de livraison">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1400017465495" ID="ID_1021417591" MODIFIED="1400017507729" TEXT="Normalis&#xe9; (voir Google)"/>
+</node>
+<node COLOR="#990000" CREATED="1400017589762" ID="ID_1163138566" MODIFIED="1400017593128" TEXT="Customer">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1400017595114" ID="ID_1981857928" MODIFIED="1400017627472" TEXT="plusieurs customer par projet"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1400017726503" ID="ID_1855562333" MODIFIED="1400017732539" TEXT="R&amp;D Amortisation">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1400017816843" ID="ID_1130421308" MODIFIED="1400017964256" TEXT="Cout total / nb annees / nb product">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1400016689768" ID="ID_850230069" MODIFIED="1400016722259" POSITION="right" TEXT="Modification">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1400016729015" ID="ID_574657570" MODIFIED="1400016731532" POSITION="left" TEXT="Dashboard">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
